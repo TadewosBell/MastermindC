006989d0624531bd5f6feff19141a36da460f36b
Tadewosbell <bell6@gmail.com>
2019-11-21 20:01:27 -0500

    read added

--- mastermind-test.c
+++ mastermind-test.c
@@ -1,5 +1,9 @@
 /* YOUR FILE-HEADER COMMENT HERE */
+/*
+help from: 
 
+	Carly Heiner
+*/
 #include <fcntl.h>
 #include <pthread.h>
 #include <stdio.h>
@@ -11,16 +15,36 @@
 
 int main(void) {
 	printf("Hello, world!\n");
-
+	int fd,retval;
 	int fileDesc;
+	void *dest;
+	char readBuf[];
+
+	fd = Open("/dev/mm",O_RDWR | O_CREAT, S_IRUSR | S_IWUSR)
+
+	dest = mmap(NULL, PAGE_SIZE,PROT_READ,MAP_SHARED,fd,0)
+
+	if(dest == MAP_FAILED){
+		printf("failed memory map");
+		exit(1);
+	}
+	munmap(dest, PAGE_SIZE);
+
+	close(fd);
+
 
 	fileDesc = open("/dev/mm_ctl", O_RDWR);
+
 	if(fileDesc < 0){
 		printf("didnt open\n");
 		return 0;
 	}
 
-	write(fileDesc, "start", 5);
+	retVal = write(fileDesc, "start\n", 5);
+
+	retVal = read(fileDesc, readBuf, 18);
+
+	printf("read result %s\n", readBuf);
 
 	close(fileDesc);
 
