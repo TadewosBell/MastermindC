4eab8a283d79b7c7271bf0c644f5fc859e199dde
TadewosBell <bell6@umbc.edu>
2019-12-15 15:09:57 -0500

    (int *)

--- mastermind2/mastermind2.c
+++ mastermind2/mastermind2.c
@@ -427,7 +427,7 @@ static struct miscdevice mm_ctl_dev = {
 
 static irqreturn_t validate_packet(void *cookie){
 	pr_info("This is validate data\n");
-	pr_info("cooke first num: %p\n", (cookie[0]));
+	pr_info("cooke first num: %p\n", (int *)cookie));
 	return IRQ_WAKE_THREAD;
 }
 
